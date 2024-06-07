3ecfe8837a186c7e825243718f5df3df6bc2f20f
hjhm2018 <henryjherreram@gmail.com>
2024-06-07 10:58:19 -0700

    Leo test

--- components/Home/OneComponent.js
+++ components/Home/OneComponent.js
@@ -1,10 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export function ExampleComponent() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <TextInput placeholder="Something" />
       <StatusBar style="auto" />
     </View>
   );
