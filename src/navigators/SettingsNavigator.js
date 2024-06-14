a6f1f96d0887746c22b7698b9594f02e7c8fd6dc
DuanC001 <370966257@qq.com>
2024-06-14 11:20:00 -0700

    Update

--- src/navigators/SettingsNavigator.js
+++ src/navigators/SettingsNavigator.js
@@ -3,6 +3,11 @@ import EditProfile from "../screens/home/EditProfile";
 import Profile from "../screens/home/Profile";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
 import ArtDetail from "../screens/home/ArtDetail"
+import Settings from "../screens/home/Settings"
+import PersonalDetails from "../screens/home/settings/PersonalDetails";
+import LocationPermission from "../screens/home/settings/LocationPermision";
+import Notifications from "../screens/home/settings/Notifications";
+import ArtistsProfile from "../screens/home/settings/ArtistsProfile";
 
 const Stack = createNativeStackNavigator();
 const TopTabs = createMaterialTopTabNavigator();
@@ -21,6 +26,11 @@ export function SettingsNavigator() {
       />
       <Stack.Screen name="ArtDetail" component={ArtDetail} />
       <Stack.Screen name="EditProfile" component={EditProfile} />
+      <Stack.Screen name="Settings" component={Settings} />
+      <Stack.Screen name="ArtistsProfile" component={ArtistsProfile} />
+      <Stack.Screen name="LocationPermission" component={LocationPermission} />
+      <Stack.Screen name="Notifications" component={Notifications} />
+      <Stack.Screen name="PersonalDetails" component={PersonalDetails} />
     </Stack.Navigator>
   );
 }
