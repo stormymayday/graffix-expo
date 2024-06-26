1bb6493e2fefd8045095f2e8a08af35db0538383
Leo Repik <repiklleonid@gmail.com>
2024-06-26 12:59:04 -0700

    Qr code to image to to Beck-end

--- src/utils/qrCodeGeneration.js
+++ src/utils/qrCodeGeneration.js
@@ -3,5 +3,10 @@ const qrcode = require("qrcode");
 export default async function generateQRCode(text) {
   console.log("Generating QR Code", text);
   const qrCode = await qrcode.toDataURL(text);
+  console.log("QR Code Generated", qrCode);
+  if (!qrCode) {
+    console.log("Error generating QR Code");
+    return null;
+  }
   return qrCode;
 }
