4d5c8d1d9244672c1709eb8ec63f1e5c826e6f8e
Leo Repik <repiklleonid@gmail.com>
2024-06-27 11:22:23 -0700

    Added real time location and fixed up some buttons also added navigation and file upload to a phone

--- src/screens/addArtWork/artWorkFormInput.js
+++ src/screens/addArtWork/artWorkFormInput.js
@@ -1,25 +1,29 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import {
   StyleSheet,
   View,
   TextInput,
-  Button,
+  TouchableOpacity,
   Alert,
   ScrollView,
   Dimensions,
+  Text,
 } from "react-native";
 import ImageUpload from "../../components/ImageUpload/ImageUpload";
 import QRCode from "react-native-qrcode-svg";
 import HorizontalScrollOptions from "../../components/HorizontallScroll/horizontallScrollCategory";
 import axios from "axios";
 import ViewShot from "react-native-view-shot";
+import * as FileSystem from "expo-file-system";
+import * as MediaLibrary from "expo-media-library";
+import * as Location from "expo-location";
 
-export default function ArtworkFormInput({ route }) {
+export default function ArtworkFormInput({ route, navigation }) {
   const { artWork } = route.params;
 
   const [categories, setCategories] = useState([
     "oil",
-    "panting",
+    "painting",
     "Abstract",
     "Nature",
     "Photo",
@@ -31,6 +35,7 @@ export default function ArtworkFormInput({ route }) {
   const [description, setDescription] = useState("");
   const [message, setMessage] = useState("");
   const [QrCodeNeeded, setQrCodeNeeded] = useState(!artWork);
+  const [location, setLocation] = useState(null);
   const viewShotRef = useRef(null);
   let treasureId = "123456";
 
@@ -41,6 +46,19 @@ export default function ArtworkFormInput({ route }) {
     flagApi = "treasure";
     flagApi2 = "treasure";
   }
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        Alert.alert("Permission to access location was denied");
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
 
   function handleImage(uri) {
     setSelectedImage(uri);
@@ -58,9 +76,13 @@ export default function ArtworkFormInput({ route }) {
     formData.append("category", selectedCategory.toLowerCase());
 
     if (QrCodeNeeded) {
+      if (!location) {
+        Alert.alert("Location not available. Please try again.");
+        return;
+      }
       formData.append("message", message);
-      formData.append("longitude", -73.935242);
-      formData.append("latitude", 40.73061);
+      formData.append("longitude", location.coords.longitude);
+      formData.append("latitude", location.coords.latitude);
     }
 
     if (selectedImage) {
@@ -100,6 +122,10 @@ export default function ArtworkFormInput({ route }) {
       } else {
         Alert.alert("Failed to publish artwork", error.message);
       }
+    } finally {
+      if (!QrCodeNeeded) {
+        navigation.navigate("ArtistsProfile");
+      }
     }
   }
 
@@ -110,7 +136,23 @@ export default function ArtworkFormInput({ route }) {
   const captureQRCode = async () => {
     if (viewShotRef.current) {
       const uri = await viewShotRef.current.capture();
+      await saveQRCodeToGallery(uri);
       uploadQRCodeImage(uri);
+    }
+  };
+
+  const saveQRCodeToGallery = async (uri) => {
+    try {
+      const { status } = await MediaLibrary.requestPermissionsAsync();
+      if (status === "granted") {
+        const asset = await MediaLibrary.createAssetAsync(uri);
+        await MediaLibrary.createAlbumAsync("Download", asset, false);
+        Alert.alert("QR code saved to gallery successfully");
+      } else {
+        Alert.alert("Permission to access gallery is required");
+      }
+    } catch (error) {
+      Alert.alert("Failed to save QR code", error.message);
     }
   };
 
@@ -145,11 +187,35 @@ export default function ArtworkFormInput({ route }) {
     <ScrollView contentContainerStyle={styles.scrollContainer}>
       <View style={styles.container}>
         {qrCodeValue ? (
-          <ViewShot ref={viewShotRef} options={{ format: "png", quality: 1.0 }}>
-            <QRCode value={qrCodeValue} size={200} />
-          </ViewShot>
+          <View style={styles.qrContainer}>
+            <Text style={styles.infoText}>
+              ARtVenture has been published, print and paste the generated QR
+              Code
+            </Text>
+            <ViewShot
+              ref={viewShotRef}
+              options={{ format: "png", quality: 1.0 }}
+            >
+              <QRCode
+                value={qrCodeValue}
+                size={200}
+                logo={{ uri: "/mnt/data/image.png" }}
+                logoSize={50}
+                logoBackgroundColor="transparent"
+              />
+            </ViewShot>
+            <TouchableOpacity style={styles.button} onPress={captureQRCode}>
+              <Text style={styles.buttonText}>Save</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.profileButton}
+              onPress={() => navigation.navigate("ArtistsProfile")}
+            >
+              <Text style={styles.buttonText}>Profile</Text>
+            </TouchableOpacity>
+          </View>
         ) : (
-          <View style={styles.container}>
+          <View style={styles.formContainer}>
             <ImageUpload handleImage={handleImage} />
             <TextInput
               style={styles.input}
@@ -180,12 +246,15 @@ export default function ArtworkFormInput({ route }) {
               options={categories}
               handleSelectCategory={handleSelectCategory}
             />
-            <Button
-              title="Publish"
-              style={styles.button}
+            <TouchableOpacity
+              style={styles.publishButton}
               onPress={handlePublish}
-            />
-            <Button title="Cancel" style={styles.cancelButton} color="#999" />
+            >
+              <Text style={styles.buttonText}>Publish</Text>
+            </TouchableOpacity>
+            <TouchableOpacity style={styles.cancelButton}>
+              <Text style={styles.buttonText}>Cancel</Text>
+            </TouchableOpacity>
           </View>
         )}
       </View>
@@ -205,6 +274,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     width: Dimensions.get("window").width,
   },
+  qrContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  formContainer: {
+    flex: 1,
+  },
   input: {
     height: 50,
     borderColor: "#ccc",
@@ -217,13 +293,47 @@ const styles = StyleSheet.create({
   textArea: {
     height: 100,
   },
+  infoText: {
+    fontSize: 16,
+    textAlign: "center",
+    marginBottom: 20,
+  },
   button: {
+    backgroundColor: "#000",
+    padding: 15,
+    marginVertical: 10,
+    width: "100%",
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  profileButton: {
+    backgroundColor: "#fff",
+    borderColor: "#000",
+    borderWidth: 1,
+    padding: 15,
+    marginVertical: 10,
+    width: "100%",
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#000",
+    fontSize: 16,
+  },
+  publishButton: {
     backgroundColor: "#4CAF50",
-    padding: 10,
-    marginHorizontal: 10,
-    borderRadius: 5,
+    padding: 15,
+    marginVertical: 10,
+    width: "100%",
+    borderRadius: 10,
+    alignItems: "center",
   },
   cancelButton: {
-    color: "#000",
+    backgroundColor: "#999",
+    padding: 15,
+    marginVertical: 10,
+    width: "100%",
+    borderRadius: 10,
+    alignItems: "center",
   },
 });
