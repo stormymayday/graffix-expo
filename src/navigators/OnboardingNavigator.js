4f274d1720de87a416f8b6345534529eb3cf8a5e
hjhm2018 <henryjherreram@gmail.com>
2024-07-03 11:58:38 -0700

    Create onboarding screens issue #76

--- src/navigators/OnboardingNavigator.js
+++ src/navigators/OnboardingNavigator.js
@@ -1,6 +1,9 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { BottomTabNavigator } from "./BottomTabNavigator";
-import Onboarding from "../screens/home/Onboarding";
+import Onboarding from "../screens/home/Onboarding/Onboarding";
+import Onboarding2 from "../screens/home/Onboarding/Onboarding2";
+import Onboarding3 from "../screens/home/Onboarding/Onboarding3";
+import Onboarding4 from "../screens/home/Onboarding/Onboarding4";
 
 const Stack = createNativeStackNavigator();
 
@@ -9,15 +12,19 @@ export function OnboardingNavigator() {
     <Stack.Navigator
       screenOptions={{
         headerTitleAlign: "center",
+        headerTitle: "Graffix",
       }}
     >
       <Stack.Screen name="Onboarding" component={Onboarding} />
+      <Stack.Screen name="Onboarding2" component={Onboarding2} />
+      <Stack.Screen name="Onboarding3" component={Onboarding3} />
+      <Stack.Screen name="Onboarding4" component={Onboarding4} />
       <Stack.Screen
         name="Graffix"
         component={BottomTabNavigator}
         options={{
           headerBackVisible: false,
-          headerShown: false
+          headerShown: false,
         }}
       />
     </Stack.Navigator>
---
+++ src/screens/home/Onboarding/Onboarding3.js
@@ -0,0 +1,71 @@
+import { StyleSheet, Text, Button, SafeAreaView, View } from "react-native";
+import { Image } from "expo-image";
+import * as Progress from "react-native-progress";
+const image = require("../../../../assets/Artboard_3.png");
+
+export default function Onboarding3({ navigation }) {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>Dive into ArtVenture</Text>
+      <Text style={styles.description}>
+        Join the ArtVenture and unlock exclusive deals and content like never
+        before!
+      </Text>
+      <Image
+        style={styles.image}
+        source={image}
+        contentFit="contain"
+        transition={1000}
+      />
+      <View style={styles.progress}>
+        <Text>
+          <Text style={{ fontWeight: "bold" }}>03</Text>/03
+        </Text>
+      </View>
+      <View style={{ paddingVertical: 10 }}>
+        <Progress.Bar progress={1} width={null} color="black" />
+      </View>
+      <Button
+        style={styles.button}
+        color={"black"}
+        title="Get Started"
+        onPress={() => navigation.navigate("Onboarding4")}
+      />
+      {/* <Button title="Start" onPress={() => navigation.navigate("Graffix")} /> */}
+      {/* <Button
+        title="Image Upload"
+        onPress={() => navigation.navigate("SelectAndUpload")}
+      /> */}
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    // alignItems: "center",
+    justifyContent: "space-between",
+    marginHorizontal: 30,
+    paddingVertical: 10,
+    padding: 0,
+  },
+  title: {
+    fontSize: 32,
+  },
+  description: {
+    fontSize: 14,
+  },
+  image: {
+    flex: 1,
+    width: "100%",
+  },
+  progress: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    margin: 5,
+  },
+  button: {
+    backgroundColor: "black",
+  },
+});
