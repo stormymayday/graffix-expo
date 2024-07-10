95c1ab47c7b7dfb1f95c283ddbfc390752802a54
stormymayday <stormymayday7@gmail.com>
2024-07-09 22:37:04 -0700

    Fixed ArtVenture Success Screen Router Reset #140

--- src/screens/artVenture/AVSuccessScreen.js
+++ src/screens/artVenture/AVSuccessScreen.js
@@ -1,10 +1,10 @@
 import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  Button,
-  Pressable,
+    StyleSheet,
+    Text,
+    View,
+    SafeAreaView,
+    Button,
+    Pressable,
 } from "react-native";
 import { CommonActions } from "@react-navigation/native";
 import { Image } from "expo-image";
@@ -12,19 +12,19 @@ import { Image } from "expo-image";
 const image = require("../../../assets/successIcon.png");
 
 export default function AVSuccessScreen({ navigation }) {
-  const handleHomePress = () => {
-    navigation.dispatch(
-      CommonActions.reset({
-        index: 0,
-        routes: [{ name: "Home" }],
-      })
-    );
-  };
+    const handleHomePress = () => {
+        navigation.dispatch(
+            CommonActions.reset({
+                index: 0,
+                routes: [{ name: "Home" }],
+            })
+        );
+    };
 
-  return (
-    <SafeAreaView style={styles.container}>
-      {/* <Text>AV Success Screen</Text> */}
-      {/*       
+    return (
+        <SafeAreaView style={styles.container}>
+            {/* <Text>AV Success Screen</Text> */}
+            {/*       
       <Button
         title="ArtVenture"
         color="black"
@@ -45,81 +45,86 @@ export default function AVSuccessScreen({ navigation }) {
         }}
       /> */}
 
-      <View style={styles.imageContainer}>
-        <Image
-          style={styles.image}
-          source={image}
-          contentFit="contain"
-          transition={1000}
-        />
-        <Text style={styles.successText}>SUCCESS</Text>
-      </View>
+            <View style={styles.imageContainer}>
+                <Image
+                    style={styles.image}
+                    source={image}
+                    contentFit="contain"
+                    transition={1000}
+                />
+                <Text style={styles.successText}>SUCCESS</Text>
+            </View>
 
-      <View>
-        <Pressable
-          style={[styles.button, styles.homeButton]}
-          onPress={() => navigation.navigate("Home")}
-        >
-          <Text style={[styles.text, styles.textHome]}>Homepage</Text>
-        </Pressable>
+            <View>
+                <Pressable
+                    style={[styles.button, styles.homeButton]}
+                    onPress={() =>
+                        // navigation.navigate("Home")
+                        handleHomePress()
+                    }
+                >
+                    <Text style={[styles.text, styles.textHome]}>Homepage</Text>
+                </Pressable>
 
-        <Pressable
-          style={[styles.button, styles.artVentureButton]}
-          onPress={() => navigation.navigate("AVTutorialScreen1")}
-        >
-          <Text style={[styles.text, styles.textArtVenture]}>ArtVenture</Text>
-        </Pressable>
-      </View>
-    </SafeAreaView>
-  );
+                <Pressable
+                    style={[styles.button, styles.artVentureButton]}
+                    onPress={() => navigation.navigate("AVTutorialScreen1")}
+                >
+                    <Text style={[styles.text, styles.textArtVenture]}>
+                        ArtVenture
+                    </Text>
+                </Pressable>
+            </View>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    justifyContent: "space-around",
-    paddingHorizontal: 16,
-  },
-  button: {
-    alignItems: "center",
-    justifyContent: "center",
-    marginVertical: 5,
-    paddingVertical: 12,
-    paddingHorizontal: 32,
-    borderRadius: 4,
-    elevation: 3,
-  },
-  text: {
-    fontSize: 16,
-    lineHeight: 21,
-    fontWeight: "bold",
-    letterSpacing: 0.25,
-  },
-  homeButton: {
-    backgroundColor: "black",
-  },
-  artVentureButton: {
-    backgroundColor: "white",
-    borderWidth: 1,
-  },
-  textHome: {
-    color: "white",
-  },
-  textArtVenture: {
-    color: "black",
-  },
-  image: {
-    flex: 1,
-    width: "50%",
-    height: 200,
-  },
-  imageContainer: {
-    height: 300,
-    alignItems: "center",
-  },
-  successText: {
-    fontSize: 32,
-    fontWeight: "bold",
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        justifyContent: "space-around",
+        paddingHorizontal: 16,
+    },
+    button: {
+        alignItems: "center",
+        justifyContent: "center",
+        marginVertical: 5,
+        paddingVertical: 12,
+        paddingHorizontal: 32,
+        borderRadius: 4,
+        elevation: 3,
+    },
+    text: {
+        fontSize: 16,
+        lineHeight: 21,
+        fontWeight: "bold",
+        letterSpacing: 0.25,
+    },
+    homeButton: {
+        backgroundColor: "black",
+    },
+    artVentureButton: {
+        backgroundColor: "white",
+        borderWidth: 1,
+    },
+    textHome: {
+        color: "white",
+    },
+    textArtVenture: {
+        color: "black",
+    },
+    image: {
+        flex: 1,
+        width: "50%",
+        height: 200,
+    },
+    imageContainer: {
+        height: 300,
+        alignItems: "center",
+    },
+    successText: {
+        fontSize: 32,
+        fontWeight: "bold",
+    },
 });
