d97c201d0462b7f5e3f8e74439f640c41934d456
stormymayday <stormymayday7@gmail.com>
2024-07-11 12:52:01 -0700

    Fetching Nearby Artists based on real user location #163

--- src/screens/home/Nearby.js
+++ src/screens/home/Nearby.js
@@ -1,121 +1,185 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import {
-  StyleSheet,
-  Text,
-  View,
-  SafeAreaView,
-  FlatList,
-  Image,
-  TouchableOpacity,
+    StyleSheet,
+    Text,
+    View,
+    SafeAreaView,
+    FlatList,
+    Image,
+    TouchableOpacity,
+    Dimensions,
 } from "react-native";
 import { haversineDistanceBetweenPoints } from "../../utils/calculateDistance";
 import MapView, { Marker } from "react-native-maps";
+import { Context as AuthContext } from "../../context/AuthContext";
+import { Ionicons } from "@expo/vector-icons";
+
+const { width } = Dimensions.get("window");
 
 export default function Nearby({ navigation, route }) {
-  const [currentLocation, setCurrentLocation] = useState({
-    latitude: 49.225084624107566,
-    longitude: -123.10763364103961,
-    latitudeDelta: 0.005,
-    longitudeDelta: 0.005,
-  });
-  const { nearbyData } = route.params;
+    const { state: authState } = useContext(AuthContext);
 
-  const renderMarkerImage = (avatar) => (
-    <Image
-      source={{ uri: avatar }}
-      style={{ width: 30, height: 30, borderRadius: 15 }}
-    />
-  );
+    const [currentLocation, setCurrentLocation] = useState({
+        latitude: authState.currentLocation.latitude,
+        longitude: authState.currentLocation.longitude,
+        latitudeDelta: 0.005,
+        longitudeDelta: 0.005,
+    });
+    const { nearbyData } = route.params;
 
-  const renderItem = ({ item }) => (
-    <View style={styles.card}>
-      <TouchableOpacity
-        onPress={() => navigation.navigate("ArtistDetail", { artist: item })}
-        style={styles.touchableOpacity}
-      >
-        <Image style={styles.image} source={{ uri: item.featuredArtUrl }} />
-        <Text style={styles.username}>{item.username}</Text>
-        <Text style={styles.distance}>
-          {haversineDistanceBetweenPoints(
-            item.location.coordinates[1],
-            item.location.coordinates[0],
-            currentLocation.latitude,
-            currentLocation.longitude
-          )}{" "}
-          m
-        </Text>
-      </TouchableOpacity>
-    </View>
-  );
+    const renderMarkerImage = (avatar) => (
+        <Image
+            source={{ uri: avatar }}
+            style={{ width: 30, height: 30, borderRadius: 15 }}
+        />
+    );
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <MapView style={styles.map} initialRegion={currentLocation}>
-        <Marker coordinate={currentLocation} title="You are here" />
-        {nearbyData.map((item) => (
-          <Marker
-            key={item._id}
-            coordinate={{
-              latitude: item.location.coordinates[1],
-              longitude: item.location.coordinates[0],
-            }}
-            title={item.username}
-            description={item.bio}
-            onPress={() =>
-              navigation.navigate("ArtistDetail", { artist: item })
-            }
-          >
-            {renderMarkerImage(item.avatar)}
-          </Marker>
-        ))}
-      </MapView>
+    const renderItem = ({ item }) => (
+        <View style={styles.cardWrapper}>
+            <View style={styles.card}>
+                <TouchableOpacity
+                    onPress={() =>
+                        navigation.navigate("ArtistDetail", { artist: item })
+                    }
+                    style={styles.touchableOpacity}
+                >
+                    <Image
+                        style={styles.image}
+                        source={{ uri: item.featuredArtUrl }}
+                    />
+                    <Text style={styles.username}>{item.username}</Text>
+                    <View style={styles.distanceContainer}>
+                        <Ionicons
+                            name="location-outline"
+                            size={16}
+                            color="black"
+                            style={styles.locationIcon}
+                        />
+                        <Text style={styles.distance}>
+                            {haversineDistanceBetweenPoints(
+                                item.location.coordinates[1],
+                                item.location.coordinates[0],
+                                currentLocation.latitude,
+                                currentLocation.longitude
+                            )}{" "}
+                            m
+                        </Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
 
-      <FlatList
-        data={nearbyData}
-        renderItem={renderItem}
-        keyExtractor={(item) => item._id}
-        ItemSeparatorComponent={<View style={{ height: 16 }} />}
-        numColumns={2}
-        columnWrapperStyle={styles.row}
-      />
-    </SafeAreaView>
-  );
+    return (
+        <SafeAreaView style={styles.container}>
+            <MapView style={styles.map} initialRegion={currentLocation}>
+                {/* <Marker coordinate={currentLocation} title="You are here" /> */}
+                {nearbyData.map((item) => (
+                    <Marker
+                        key={item._id}
+                        coordinate={{
+                            latitude: item.location.coordinates[1],
+                            longitude: item.location.coordinates[0],
+                        }}
+                        title={item.username}
+                        description={item.bio}
+                        onPress={() =>
+                            navigation.navigate("ArtistDetail", {
+                                artist: item,
+                            })
+                        }
+                    >
+                        {renderMarkerImage(item.avatar)}
+                    </Marker>
+                ))}
+            </MapView>
+
+            <FlatList
+                style={styles.listContainer}
+                data={nearbyData}
+                renderItem={renderItem}
+                keyExtractor={(item) => item._id}
+                ItemSeparatorComponent={<View style={{ height: 0 }} />}
+                numColumns={2}
+                columnWrapperStyle={styles.row}
+                ListHeaderComponent={() => (
+                    <View
+                        style={
+                            nearbyData.length % 2 !== 0
+                                ? styles.emptyCard
+                                : null
+                        }
+                    />
+                )}
+            />
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-  },
-  map: {
-    width: "100%",
-    height: "40%",
-  },
-  row: {
-    flex: 1,
-    justifyContent: "space-around",
-  },
-  card: {
-    flex: 1,
-    margin: 10,
-    height: 200,
-    alignItems: "center",
-  },
-  touchableOpacity: {
-    flex: 1,
-    width: "100%", 
-    alignItems: "center", 
-  },
-  image: {
-    width: "100%",
-    height: 140, 
-  },
-  username: {
-    fontWeight: "bold",
-    textTransform: "capitalize",
-    paddingTop: 5,
-  },
-  distance: {
-    paddingTop: 5,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+    },
+    map: {
+        width: "100%",
+        height: "40%",
+    },
+    listContainer: {
+        flex: 1,
+        backgroundColor: "#fff",
+        borderTopLeftRadius: 15,
+        borderTopRightRadius: 15,
+        zIndex: 1,
+        marginTop: -20,
+        paddingTop: 20,
+        paddingHorizontal: 10,
+    },
+    row: {
+        flex: 1,
+        justifyContent: "flex-start",
+        marginBottom: 15,
+    },
+    cardWrapper: {
+        width: "50%",
+        padding: 5,
+        paddingBottom: 0,
+    },
+    card: {
+        flex: 1,
+        height: 200,
+        overflow: "hidden",
+    },
+    emptyCard: {
+        width: "50%",
+        height: 0,
+    },
+    touchableOpacity: {
+        flex: 1,
+        width: "100%",
+    },
+    image: {
+        width: "100%",
+        height: 140,
+        borderRadius: 8,
+        marginBottom: 8,
+    },
+    cardContent: {
+        padding: 8,
+    },
+    username: {
+        fontWeight: "bold",
+        textTransform: "capitalize",
+        marginBottom: 4,
+    },
+    distanceContainer: {
+        flexDirection: "row",
+        alignItems: "center",
+    },
+    locationIcon: {
+        marginRight: 4,
+    },
+    distance: {
+        color: "gray",
+    },
 });
