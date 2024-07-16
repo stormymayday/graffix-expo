360eb7cd9086f089db042bd5d066753b9ed05219
Leo Repik <repiklleonid@gmail.com>
2024-07-16 11:07:33 -0700

    Updated Atr work upload Qrcode download save Item lots of styling

--- src/screens/addArtWork/selectAndUpload.js
+++ src/screens/addArtWork/selectAndUpload.js
@@ -1,11 +1,18 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+} from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
 export default function SelectAndUpload(props) {
   const { artWork } = props;
   const [isArtWork, setIsArtWork] = useState(artWork);
   const navigation = useNavigation();
+  const screenHeight = Dimensions.get("window").height;
 
   useEffect(() => {
     setIsArtWork(artWork);
@@ -13,7 +20,7 @@ export default function SelectAndUpload(props) {
 
   const renderCircle = (isActive) => (
     <View
-      style={[styles.circle, { backgroundColor: isActive ? "green" : "grey" }]}
+      style={[styles.circle, { backgroundColor: isActive ? "black" : "grey" }]}
     />
   );
 
@@ -30,31 +37,35 @@ export default function SelectAndUpload(props) {
   };
 
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { paddingTop: screenHeight * 0.1 }]}>
       <Text style={styles.heading}>Select and Upload</Text>
       <Text style={styles.paragraph}>
-        Choose to upload to your ArtWork or to ArtVenture
+        Choose to upload your ArtWork or ArtVenture
       </Text>
 
-      <TouchableOpacity
-        style={styles.inputContainer}
-        onPress={handleArtWorkPress}
-      >
-        {renderCircle(isArtWork)}
-        <Text>ArtWork</Text>
-      </TouchableOpacity>
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={handleArtWorkPress}
+          style={styles.radioButton}
+        >
+          {renderCircle(isArtWork)}
+          <Text style={styles.optionText}>ArtWork</Text>
+        </TouchableOpacity>
 
-      <TouchableOpacity
-        style={styles.inputContainer}
-        onPress={handleArtVenturePress}
-      >
-        {renderCircle(!isArtWork)}
-        <Text>ArtVenture</Text>
-      </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handleArtVenturePress}
+          style={styles.radioButton}
+        >
+          {renderCircle(!isArtWork)}
+          <Text style={styles.optionText}>ArtVenture</Text>
+        </TouchableOpacity>
+      </View>
 
-      <TouchableOpacity style={styles.button} onPress={handleContinuePress}>
-        <Text style={styles.buttonText}>Continue</Text>
-      </TouchableOpacity>
+      <View style={styles.continueButtonContainer}>
+        <TouchableOpacity style={styles.button} onPress={handleContinuePress}>
+          <Text style={styles.buttonText}>Continue</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -62,42 +73,72 @@ export default function SelectAndUpload(props) {
 const styles = StyleSheet.create({
   container: {
     padding: 20,
+    alignItems: "center",
+    flex: 1,
   },
   heading: {
-    fontSize: 20,
+    fontSize: 32,
     fontWeight: "bold",
-    marginBottom: 10,
-    color: "#333",
+    marginBottom: 20,
+    color: "#202020",
   },
   paragraph: {
-    fontSize: 16,
-    lineHeight: 24,
-    color: "#333",
-    marginBottom: 10,
+    fontSize: 12,
+    lineHeight: 16,
+    color: "#202020",
+    marginBottom: 40,
   },
-  inputContainer: {
+
+  buttonContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FFFFFF",
+    boxShadow: "0px 4px 4px 0px #00000040",
+    shadowRadius: 4,
+    borderRadius: 8,
+    padding: 24,
+    gap: 20,
+  },
+  radioButton: {
     flexDirection: "row",
     alignItems: "center",
-    borderColor: "#000",
+    borderColor: "grey",
     borderWidth: 1,
+    borderRadius: 8,
+    padding: 10,
+    width: 244,
+    height: 44,
     marginBottom: 10,
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    height: 40,
+    justifyContent: "center",
   },
   circle: {
     width: 20,
     height: 20,
     borderRadius: 10,
     marginRight: 10,
+    position: "absolute",
+    left: 10,
+  },
+  optionText: {
+    fontSize: 16,
+    fontWeight: "600",
+    color: "grey",
+  },
+  continueButtonContainer: {
+    position: "absolute",
+    bottom: 30,
+    width: "100%",
+    paddingHorizontal: 20,
   },
   button: {
-    backgroundColor: "#007bff",
-    padding: 10,
-    marginVertical: 5,
+    backgroundColor: "#202020",
+    padding: 16,
+    borderRadius: 5,
     alignItems: "center",
   },
   buttonText: {
     color: "#fff",
+    fontSize: 20,
+    fontWeight: "600",
   },
 });
