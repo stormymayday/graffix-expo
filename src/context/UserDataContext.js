f9d10921943dc0e5888ef5f4c1e7cb6f69ad45f3
stormymayday <stormymayday7@gmail.com>
2024-07-15 21:03:35 -0700

    Moved the add treasure to collection functionality to userDataContext.js

--- src/context/UserDataContext.js
+++ src/context/UserDataContext.js
@@ -4,44 +4,69 @@ import graffixAPI from "../api/graffixAPI";
 const UserDataContext = createContext();
 
 export const UserDataProvider = ({ children }) => {
-  const [userData, setUserData] = useState(null);
+    const [userData, setUserData] = useState(null);
 
-  const fetchUserData = async () => {
-    try {
-      const response = await graffixAPI.get("/api/v1/users/current-user");
-      const userDataWithRole = {
-        ...response.data.userWithoutPassword,
-        role: response.data.userWithoutPassword.role || "collector", // Default role is collector if not provided
-      };
-      setUserData(userDataWithRole);
-    } catch (error) {
-      console.error("Error fetching user data:", error);
-    }
-  };
+    const fetchUserData = async () => {
+        try {
+            const response = await graffixAPI.get("/api/v1/users/current-user");
+            const userDataWithRole = {
+                ...response.data.userWithoutPassword,
+                role: response.data.userWithoutPassword.role || "collector", // Default role is collector if not provided
+            };
+            setUserData(userDataWithRole);
+        } catch (error) {
+            console.error("Error fetching user data:", error);
+        }
+    };
 
-  const updateUser = async (updatedUserData) => {
-    try {
-      const response = await graffixAPI.patch(
-        "/api/v1/users/update-user",
-        updatedUserData
-      );
-      setUserData((prevUserData) => ({
-        ...prevUserData,
-        ...updatedUserData,
-      }));
-      console.log("User data updated:", response.data);
-    } catch (error) {
-      console.error("Error updating user data:", error);
-    }
-  };
+    const updateUser = async (updatedUserData) => {
+        try {
+            const response = await graffixAPI.patch(
+                "/api/v1/users/update-user",
+                updatedUserData
+            );
+            setUserData((prevUserData) => ({
+                ...prevUserData,
+                ...updatedUserData,
+            }));
+            console.log("User data updated:", response.data);
+        } catch (error) {
+            console.error("Error updating user data:", error);
+        }
+    };
 
-  return (
-    <UserDataContext.Provider
-      value={{ userData, fetchUserData, updateUser}}
-    >
-      {children}
-    </UserDataContext.Provider>
-  );
+    const addTreasureToCollection = async (treasureId) => {
+        try {
+            const response = await graffixAPI.post(
+                `/api/v1/treasure/add-to-collection/${treasureId}`
+            );
+
+            if (response.status === 200) {
+                setUserData((prevUserData) => ({
+                    ...prevUserData,
+                    treasureCollection: response.data.treasureCollection,
+                }));
+            }
+
+            return response;
+        } catch (error) {
+            console.error("Error adding treasure to collection:", error);
+            throw error;
+        }
+    };
+
+    return (
+        <UserDataContext.Provider
+            value={{
+                userData,
+                fetchUserData,
+                updateUser,
+                addTreasureToCollection,
+            }}
+        >
+            {children}
+        </UserDataContext.Provider>
+    );
 };
 
 export default UserDataContext;
