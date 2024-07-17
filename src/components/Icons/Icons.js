7b02e98c4a1a06acfff0abff4834a5dc64f744b9
hjhm2018 <henryjherreram@gmail.com>
2024-07-17 10:01:25 -0700

    Replace Ionicons for SVG icons at home search and categories screens issue #202

---
+++ src/components/Icons/Icons.js
@@ -0,0 +1,65 @@
+import { Image } from "expo-image";
+
+const chevronForwardIcon = require("../../../assets/Icons/chevron_forward.svg");
+const favoriteIcon = require("../../../assets/Icons/favorite.svg");
+const mapPinIcon = require("../../../assets/Icons/map_pin.svg");
+const outlineHeartIcon = require("../../../assets/Icons/outline_heart.svg");
+const globeIcon = require("../../../assets/Icons/Position.svg");
+const searchIcon = require("../../../assets/Search/Filled.svg");
+
+export function ChevronForward({ size, color }) {
+  return (
+    <Image
+      source={chevronForwardIcon}
+      style={{ width: size, height: size, tintColor: color }}
+    />
+  );
+}
+
+export function Favorite({ size, color }) {
+  return (
+    <Image
+      source={favoriteIcon}
+      contentFit="contain"
+      style={{ width: size, height: size, tintColor: color, marginRight: 5 }}
+    />
+  );
+}
+
+export function MapPin({ size, color }) {
+  return (
+    <Image
+      source={mapPinIcon}
+      contentFit="contain"
+      style={{ width: size, height: size, tintColor: color }}
+    />
+  );
+}
+
+export function OutlineHeart({ size, color }) {
+  return (
+    <Image
+      source={outlineHeartIcon}
+      contentFit="contain"
+      style={{ width: size, height: size, tintColor: color }}
+    />
+  );
+}
+
+export function Globe({ size, color }) {
+  return (
+    <Image
+      source={globeIcon}
+      style={{ width: size, height: size, tintColor: color }}
+    />
+  );
+}
+
+export function Search({ size, color }) {
+  return (
+    <Image
+      source={searchIcon}
+      style={{ width: size, height: size, tintColor: color }}
+    />
+  );
+}
